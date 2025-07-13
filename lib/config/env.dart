204625ae9c488a2766c759c4e47456a106458b02
Daniel Collaguazo Malla <collaguazodaniel48@gmail.com>
2025-07-12 20:45:56 -0500

    Remove env.dart from tracking

--- lib/config/env.dart
+++
@@ -1,7 +0,0 @@
-class Env {
-  static const bool isProduction = false;
-
-  static const String apiBaseUrl = 'https://s2vj0zmk-5001.brs.devtunnels.ms/api';
-
-  static const String dateFormat = 'dd/MM/yyyy';
-}
